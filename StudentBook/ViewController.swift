450b5760e07483af901b78d786bb840d18f27e67
Sahand Nayebaziz <sahand@sahand.me>
2015-11-15 08:33:02 -0800

    Initial Commit

---
+++ StudentBook/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  StudentBook
+//
+//  Created by Sahand Nayebaziz on 11/15/15.
+//  Copyright © 2015 Sahand Nayebaziz. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
